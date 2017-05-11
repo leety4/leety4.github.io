7b5147c0ce412574bb75e5695a8aba7203241fe0
leety4 <leety4@comcast.net>
2017-05-11 12:00:59 -0500

    Update lighting.js

--- lighting.js
+++ lighting.js
@@ -2,7 +2,7 @@ var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 ctx.fillStyle = "30a0ff00";
 ctx.noStroke();
-var loadfunct = function{
+var loadfunct = function(){
   for(var x = 0; x < c.width; x++){
     for(var y = 0; y < c.height; y++){
       ctx.fillRect(x,y,1,1);
